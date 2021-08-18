d02277c2b3ada39cef32c2aee7c62f3360a1e366
maythingo <maythingo@aim.com>
2021-08-17 20:44:23 -0700

    bootstrap for index.html and started js

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var cityInput = document.querySelector('#city-input');
+var cityBtn = document.querySelector('#search-btn');
+var cityNameEl = document.querySelector('#city-name');
+var cityArr = [];
+var apiKey = 'e954f2248f890ef53e2612dbcce58559';
+
+var formHandler = function(event) {
